e72422a819700ae1855f8ab977af760ddda12a37
Nunticha <66160135@go.buu.ac.th>
2026-07-11 20:23:13 +0700

    update users.sql

--- database/users.sql
+++ database/users.sql
@@ -7,7 +7,11 @@ CREATE TABLE users (
   profile_image VARCHAR(255)
 );
 
-INSERT INTO users (username,password,name,role,profile_image)
-VALUES
-('66160000','1234','สุขใจ ใจดี','student','/src/assets/student.jpg'),
-('teacher01','1234','ผศ.ดร.สมชาย ใจดี','teacher','/src/assets/teacher01.jpg');
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `role`, `profile_image`) VALUES
+(1, '66160000', '1234', 'สุขใจ ใจดี', 'student', '/src/assets/student.jpg'),
+(2, 'teacher01', '1234', 'ผศ.ดร.สมชาย ใจดี', 'teacher', '/src/assets/teacher01.jpg'),
+(3, 'teacher02', '1234', 'ผศ.ดร.กิตติ', 'teacher', '/src/assets/teacher02.jpg'),
+(4, 'teacher03', '1234', 'ผศ.ดร.ประวิทย์', 'teacher', '/src/assets/teacher03.jpg'),
+(5, 'teacher04', '1234', 'ผศ.ดร.สุชาติ', 'teacher', '/src/assets/teacher04.jpg'),
+(6, 'teacher05', '1234', 'ผศ.ดร.อนันต์', 'teacher', '/src/assets/teacher05.jpg'),
+(7, '66160001', '1234', 'กิตติพงษ์ แสงดี', 'student', '/src/assets/student2.jpg');
